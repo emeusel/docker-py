e3cd12cc1ca943a6d836b13a025d0fe196e22bfb
Erik Meusel <em.opensource@gmail.com>
2017-03-19 10:14:58 +0100

    disable error in order to make repetitive package building possible

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ from setuptools import setup, find_packages
 
 if 'docker-py' in [x.project_name for x in pip.get_installed_distributions()]:
     print(
-        'ERROR: "docker-py" needs to be uninstalled before installing this'
+        'WARNING: "docker-py" needs to be uninstalled before installing this'
         ' package:\npip uninstall docker-py', file=sys.stderr
     )
-    sys.exit(1)
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
